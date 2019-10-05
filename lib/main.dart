429b0a07d5b2471b9c0eb692ab45bdf4f5ec293f
waspryo <wasp2123321@gmail.com>
2019-10-05 13:45:02 +0900

    styling text

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'customIcons.dart';
 
 void main() => runApp(MaterialApp(
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-)); // MaterialApp
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    )); // MaterialApp
 
-class MyApp extends StatefulWidget { 
+class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => new _MyAppState();
 }
@@ -14,15 +14,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       backgroundColor: Color(0xFF2d3447),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.only(
-                left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+                  left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   IconButton(
                     icon: Icon(
@@ -30,7 +31,7 @@ class _MyAppState extends State<MyApp> {
                       color: Colors.white,
                       size: 30.0,
                     ),
-                    onPressed: (){},
+                    onPressed: () {},
                   ),
                   IconButton(
                     icon: Icon(
@@ -38,10 +39,54 @@ class _MyAppState extends State<MyApp> {
                       color: Colors.white,
                       size: 30.0,
                     ),
-                    onPressed: (){},
+                    onPressed: () {},
                   )
                 ],
-                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Trending',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 46.0,
+                        fontFamily: "Calibre-Semibold",
+                        letterSpacing: 1.0,
+                      )),
+                  IconButton(
+                    icon: Icon(
+                      CustomIcons.option,
+                      size: 12.0,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFff6e6e),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 22.0, vertical: 6.0),
+                        child: Text("Animated",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
