b0875edb0743c57d658e923c2ac4d6ae52803675
waspryo <wasp2123321@gmail.com>
2019-10-05 15:13:16 +0900

    CatdItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'customIcons.dart';
+import 'data.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -11,9 +13,21 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => new _MyAppState();
 }
 
+var cardAspectRatio = 12.0 / 16.0;
+var widgetAspectRatio = cardAspectRatio * 1.2;
+
 class _MyAppState extends State<MyApp> {
+  var currentPage = images.length - 1.0;
+
   @override
   Widget build(BuildContext context) {
+    PageController controller = PageController(initialPage: images.length - 1);
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page;
+      });
+    });
+
     return Scaffold(
       backgroundColor: Color(0xFF2d3447),
       body: SingleChildScrollView(
@@ -78,18 +92,94 @@ class _MyAppState extends State<MyApp> {
                     ),
                     child: Center(
                       child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 22.0, vertical: 6.0),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 22.0, vertical: 6.0),
                         child: Text("Animated",
                             style: TextStyle(color: Colors.white)),
                       ),
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Text("25+ Stories",
+                      style: TextStyle(color: Colors.blueAccent))
                 ],
               ),
+            ),
+            Stack(
+              children: <Widget>[
+                CardScrollWidget(currentPage),
+                Positioned.fill(
+                    child: PageView.builder(
+                  itemCount: images.length,
+                  controller: controller,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    return Container();
+                  },
+                ))
+              ],
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CardScrollWidget extends StatelessWidget {
+  var currentPage;
+  var padding = 20.0;
+  var verticalInset = 20.0;
+
+  CardScrollWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(
+        builder: (context, contraints) {
+          var width = contraints.maxWidth;
+          var height = contraints.maxHeight;
+
+          var safeWidth = width - 2 * padding;
+          var safeHeight = height - 2 * padding;
+
+          var heightOfPrimaryCard = safeHeight;
+          var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+          var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+          var horizontalInset = primaryCardLeft / 2;
+
+          for (var i = 0; i < images.length; i++) {
+            var delta = i - currentPage;
+            bool isOnRight = delta > 0;
+
+            var start = padding +
+                max(
+                    primaryCardLeft -
+                        horizontalInset * -delta * (isOnRight ? 15 : 1),
+                    0.0);
+
+            var cardItem = Positioned.directional(
+              top: padding + verticalInset * max(-delta, 0.0),
+              bottom: padding + verticalInset * max(-delta, 0.0),
+              start: start,
+              textDirection: TextDirection.ltr,
+              child: Container(
+                child: AspectRatio(
+                  aspectRatio: cardAspectRatio,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[Image.asset(images[i], fit: BoxFit.cover)],
+                  ),
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
